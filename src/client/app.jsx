d960b732a22eda4b5e17e1e6a247d60b64ed4ac5
Richard Shea <rshea@thecubagroup.com>
2017-10-22 18:46:51 +1300

    Install Reaact and make some use of it. This commit fails the yarn lint tests but as we're about to change this file in the next commit I think we'll let it go

---
+++ src/client/app.jsx
@@ -0,0 +1,7 @@
+// @flow
+
+import React from 'react'
+
+const App = () => <h1>Hello Something else !</h1>
+
+export default App
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,11 @@
+// @flow
+
 import 'babel-polyfill'
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './app'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 
-document.querySelector(APP_CONTAINER_SELECTOR).innerHTML = '<h1>Hello Webpack!!</h1>'
+ReactDOM.render(<App />, document.querySelector(APP_CONTAINER_SELECTOR))
