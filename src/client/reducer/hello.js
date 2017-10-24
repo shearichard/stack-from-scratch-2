10eb38f109bb39eedd7af2d4b82348db79ca2bf7
Richard Shea <rshea@thecubagroup.com>
2017-10-24 15:00:41 +1300

    After installing Redux and creating a simple action and reducer

---
+++ src/client/reducer/hello.js
@@ -0,0 +1,23 @@
+// @flow
+
+import Immutable from 'immutable'
+import type { fromJS as Immut } from 'immutable'
+
+import { SAY_HELLO } from '../action/hello'
+
+const initialState = Immutable.fromJS({
+  message: 'Initial reducer message',
+})
+
+const helloReducer = (state: Immut = initialState, action: { type: string, payload: any }) => {
+  //Reducers are just pure functions that take the previous 
+  //state and an action, and return the next state. 
+  switch (action.type) {
+    case SAY_HELLO:
+      return state.set('message', action.payload)
+    default:
+      return state
+  }
+}
+
+export default helloReducer
